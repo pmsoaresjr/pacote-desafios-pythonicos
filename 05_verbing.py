e02c37fe33955535da9ff2329b9121bfbb400705
Paulo Marcio <paulo.soares@resale.com.br>
2021-08-22 23:53:38 -0300

    feat: solução do 5  desafio

--- 05_verbing.py
+++ 05_verbing.py
@@ -10,9 +10,14 @@ Se o tamanho da string for menor que 3, não altere nada.
 Retorne o resultado da string.
 """
 
+
 def verbing(s):
     # +++ SUA SOLUÇÃO +++
-    return
+    if len(s) > 2 and s[-3:] != "ing":
+        s += "ing"
+    elif len(s) > 2 and s[-3:] == "ing":
+        s += "ly"
+    return s
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
